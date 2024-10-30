d3806a78b2fa69be12f5733f33be0d1bfff3725c
Yers <eabuov4@gmail.com>
2024-10-30 23:57:11 +0500

    fixing vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@mui/material", "@emotion/react", "@emotion/styled"],
+  },
   define: {
-    'process.env': process.env
-  }
-})
+    "process.env": process.env,
+  },
+  server: {
+    port: 3000,
+  },
+});
