b7906d56248d9dc258c5de6e76f603fdeabc853f
Yers <eabuov4@gmail.com>
2025-02-07 03:23:57 +0500

    refined marquee

--- src/components/layouts/default-layout.tsx
+++ src/components/layouts/default-layout.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment, ReactNode } from "react";
 import { useLocation } from "react-router-dom";
-import Brands from "../brands/brands";
+import Brands from "../BrandsMarquee";
 import Carousels from "../carousels";
 import Footer from "./Footer";
 import Meta from "../UI/meta";
